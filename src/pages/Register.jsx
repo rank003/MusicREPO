e8394fe5d5200ed1df49b183538930b1fbe06c1f
rank003 <fokolie8@gmail.com>
2024-10-16 04:47:37 +0100

    new frontend eal test

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -14,7 +14,7 @@ const Register = () => {
     e.preventDefault();
     
     try {
-      const response = await axios.post('http://localhost:5000/api/auth/register', {
+      const response = await axios.post('https://musicrepobcknd.onrender.com:5000/api/auth/register', {
         username,
         email,
         password
